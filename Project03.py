8b7459ffc0aca5a271b45ff1ed80946c0c3d2e2f
aditya-python-projects <adityasawaimul8@gmail.com>
2026-04-10 18:35:38 +0530

    Add files via upload

---
+++ Project03.py
@@ -0,0 +1,27 @@
+# Project03.py
+
+def main():
+    print("Welcome to Project 03")
+
+    #Example: simple calculator
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    operation = input("Choose operation (+, -, *, /): ")
+
+    if operation == "+":
+        print("Result",num1 + num2)
+    elif operation == "-": 
+        print("Result", num1 - num2)   
+    elif operation == "*": 
+        print("Result:", num1 * num2) 
+    elif operation == "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Cannot divide by zero")       
+    else:
+        print("Invalid operation")            
+
+if__name__=="__main__":
+    main()        
